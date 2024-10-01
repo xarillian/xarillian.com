ff6ca323a5f4adcee61d87fe0dc11ca8c4352367
xarillian <austinwheinrich@gmail.com>
2024-10-01 00:02:38 -0600

    loads of style changes, added a menu

--- app.py
+++ app.py
@@ -1,18 +1,54 @@
 import random
-from flask import Flask, render_template
+import os
+from flask import (
+  Flask,
+  render_template,
+  render_template_string, 
+  request,
+  session,
+)
 
 from const import QUOTES
 
 app = Flask(__name__)
+
+app.secret_key = os.urandom(24)
 
 
 @app.route('/')
 def home():
   return render_template('index.html')
 
+
 @app.route('/random-quote')
 def random_quote():
   return random.choice(QUOTES)
+
+
+@app.route('/menu')
+def menu():
+    if request.headers.get('HX-Request') == 'true':
+        session['menu_visible'] = not session.get('menu_visible', False)
+    is_visible = session.get('menu_visible', True)
+
+    menu_items = [
+        {'link': 'https://github.com/xarillian', 'display': 'github'},
+    ]
+
+    menu_html = render_template_string(
+        """
+        <ul {% if is_visible %}class="visible"{% endif %}>
+        {% for item in menu_items %}
+            <li><a href="{{ item.link }}" target="_blank">{{ item.display }}</a></li>
+        {% endfor %}
+        </ul>
+        """,
+        menu_items=menu_items,
+        is_visible=is_visible
+    )
+
+    return menu_html
+
 
 if __name__ == '__main__':
   app.run(debug=True)
--- const.py
+++ const.py
@@ -1,5 +1,6 @@
 QUOTES = [
   'it\'s not the user\'s fault',
   'it\'s not the mountains we conquer, but ourselves',
-  'a ship in a harbour is safe, but that is not what ships are built for', 
+  'a ship in a harbour is safe, but that is not what ships are built for',
+  'everything excellent is as difficult as it is rare',
 ]
