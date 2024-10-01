d07096b7f8ffb031e8df6d710fcc12e8b299d541
xarillian <austinwheinrich@gmail.com>
2024-10-01 00:47:09 -0600

    further style updates

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from flask import (
   Flask,
   render_template,
-  render_template_string, 
+  render_template_string,
   request,
   session,
 )
@@ -13,6 +13,8 @@ from const import QUOTES
 app = Flask(__name__)
 
 app.secret_key = os.urandom(24)
+if not app.secret_key:
+    raise ValueError("No SECRET_KEY set for Flask application")
 
 
 @app.route('/')
@@ -28,7 +30,7 @@ def random_quote():
 @app.route('/menu')
 def menu():
     if request.headers.get('HX-Request') == 'true':
-        session['menu_visible'] = not session.get('menu_visible', False)
+      session['menu_visible'] = not session.get('menu_visible', False)
     is_visible = session.get('menu_visible', True)
 
     menu_items = [
@@ -36,15 +38,15 @@ def menu():
     ]
 
     menu_html = render_template_string(
-        """
-        <ul {% if is_visible %}class="visible"{% endif %}>
+      """
+        <ul>
         {% for item in menu_items %}
             <li><a href="{{ item.link }}" target="_blank">{{ item.display }}</a></li>
         {% endfor %}
         </ul>
-        """,
-        menu_items=menu_items,
-        is_visible=is_visible
+      """,
+      menu_items=menu_items,
+      is_visible=is_visible
     )
 
     return menu_html
