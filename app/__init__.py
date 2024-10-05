696ea7be93a7c5f6dc96d514dcece7ed22272b90
xarillian <austinwheinrich@gmail.com>
2024-10-05 14:50:47 -0600

    disabling pylint warning

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
+
 from config import Config
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     with app.app_context():
-        from . import routes
+        from . import routes  # pylint: disable=C0415
         app.register_blueprint(routes.main)
 
     return app
