cb2381098b9207a6a9ad7009303686a54db6ecd3
xarillian <austinwheinrich@gmail.com>
2024-10-14 21:53:10 -0600

    remember remember

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
       Collection of configuration settings for xarillian.com.
 
       Attributes:
-        - SECRET_KEY (str): Securely stores session data.
+        - SECRET_KEY (str): Securely stores session data. Note: this currently uses a random
+                            value. This means session data will not persist.
     """
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
