550d2f9c8621d3d8a53c5e857faaee354885bdab
xarillian <austinwheinrich@gmail.com>
2025-01-27 21:47:39 -0600

    this one should be 2 spaces, as well

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,11 @@ from config import Config
 
 
 def create_app():
-    app = Flask(__name__, static_folder='static', static_url_path='/')
-    app.config.from_object(Config)
+  app = Flask(__name__, static_folder='static', static_url_path='/')
+  app.config.from_object(Config)
 
-    with app.app_context():
-        from . import routes  # pylint: disable=C0415
-        app.register_blueprint(routes.main)
+  with app.app_context():
+    from . import routes  # pylint: disable=C0415
+    app.register_blueprint(routes.main)
 
-    return app
+  return app
